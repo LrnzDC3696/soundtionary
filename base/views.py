1fdbc68d944ff6cbbdaf9e62e6df093daa5a5cbb
Sayed Mohammed Nouman <74371107+Skyhero-admin@users.noreply.github.com>
2022-09-03 08:05:36 +0000

    feat: added Tailwind CSS and Daisy UI & used dicebear api

--- base/views.py
+++ base/views.py
@@ -1,6 +1,13 @@
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+import requests;
 
 # Create your views here.
 
 def index(request):
     return render(request, 'base/index.html')
+
+def avatars(request):
+    return "https://avatars.dicebear.com/api/adventurer/lol.svg"
+
+

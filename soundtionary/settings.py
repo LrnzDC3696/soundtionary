08e15ba425386b068edd980ce47e18907dd4a83d
LrnzdDC3696 <lrnzdc3696@outlook.com>
2022-09-03 10:36:20 +0800

    added base app

--- soundtionary/settings.py
+++ soundtionary/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = ["localhost"]
 # Application definition
 
 INSTALLED_APPS = [
+    'base',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
